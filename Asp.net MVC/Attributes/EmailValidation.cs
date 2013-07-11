f3c277f0400fdfc3e7f926da2e28b3128b111174
SimonB <budsi_fr@hotmail.com>
2013-07-11 12:03:20 +0200

    First Commit

---
+++ Asp.net MVC/Attributes/EmailValidation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+
+namespace Tools.Asp.net_MVC.Attributes
+{
+	public class EmailValidationAttribute : ValidationAttribute
+	{
+		private readonly bool _isRequired;
+		private readonly string _requiredErrorMessage;
+		private readonly string _malformedErrorMessage;
+
+
+		public EmailValidationAttribute(bool isRequired, string requiredErrorMessage, string malformedErrorMessage)
+		{
+			_isRequired = isRequired;
+			_requiredErrorMessage = requiredErrorMessage;
+			_malformedErrorMessage = malformedErrorMessage;
+		}
+
+		protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+		{
+			try
+			{
+				if (string.IsNullOrWhiteSpace((string)value) && !_isRequired)
+					return ValidationResult.Success;
+				if (string.IsNullOrWhiteSpace((string)value) && _isRequired)
+					return new ValidationResult(_requiredErrorMessage);
+				new MailAddress((string)value);
+				return ValidationResult.Success;
+			}
+			catch (FormatException)
+			{
+				return new ValidationResult(_malformedErrorMessage);
+			}
+		}
+	}
+}
